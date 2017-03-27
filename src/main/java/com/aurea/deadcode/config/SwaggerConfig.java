f0bdfff2b52bc2d674202ec30030406e20116783
Ilshat Latypov <latypov.ilshat@gmail.com>
2017-03-27 17:38:18 +0300

    add swagger docs

---
+++ src/main/java/com/aurea/deadcode/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.aurea.deadcode.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by ilshat on 27.03.17.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Dead Code Detector",
+                "Some custom description of API.",
+                "API TOS",
+                "Terms of service",
+                new Contact("Ilshat Latypov", null, "ilshat.latypov@aurea.com"),
+                "License of API",
+                "API license URL");
+        return apiInfo;
+    }
+}
