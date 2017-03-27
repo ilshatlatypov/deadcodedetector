73d93c449aedfbd0bd52cf8f764b5602f5637ace
Ilshat Latypov <latypov.ilshat@gmail.com>
2017-03-27 09:13:47 +0300

    initial

---
+++ src/test/java/com/aurea/deadcode/DeadCodeDetectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aurea.deadcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeadCodeDetectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
